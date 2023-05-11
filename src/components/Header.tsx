8b3ce0d5f1c50adbab7cc0f7fd6695d591a98154
Alan <alan.massar@gmail.com>
2023-05-11 23:24:50 +0200

    Hero Responsive Design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   const isLoggedIn = !!session.data;
   return (
     <header className="flex h-16 items-center justify-between px-4 dark:bg-gray-800">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex items-center justify-between">
         <PrimaryLink href="/">GenIco</PrimaryLink>
         <nav>
           <ul>
@@ -16,33 +16,32 @@ const Header = () => {
               <PrimaryLink href="/generate">Generate</PrimaryLink>
             </li>
           </ul>
-          <ul>
-            {isLoggedIn && (
-              <li className="flex gap-2">
-                <Button
-                  variant="secondary"
-                  onClick={() => {
-                    signOut().catch(console.error);
-                  }}
-                >
-                  Logout
-                </Button>
-              </li>
-            )}
-            {!isLoggedIn && (
-              <li className="flex gap-2">
-                <Button
-                  onClick={() => {
-                    signIn().catch(console.error);
-                  }}
-                >
-                  Logout
-                </Button>
-              </li>
-            )}
-          </ul>
         </nav>
-        <div>Account buttons</div>
+        <ul>
+          {isLoggedIn && (
+            <li className="flex gap-2">
+              <Button
+                variant="secondary"
+                onClick={() => {
+                  signOut().catch(console.error);
+                }}
+              >
+                Logout
+              </Button>
+            </li>
+          )}
+          {!isLoggedIn && (
+            <li className="flex gap-2">
+              <Button
+                onClick={() => {
+                  signIn().catch(console.error);
+                }}
+              >
+                Logout
+              </Button>
+            </li>
+          )}
+        </ul>
       </div>
     </header>
   );
